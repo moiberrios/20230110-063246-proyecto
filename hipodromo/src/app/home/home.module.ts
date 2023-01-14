10f00304cf29254894e406a747a460782afd3420
moiberrios <sesberrios456@gmail.com>
2023-01-14 05:10:43 -0400

    20230114 045704 cambios para que sea colaborativo

---
+++ hipodromo/src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+import { FormsModule } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { HomePage } from './home.page';
+
+import { HomePageRoutingModule } from './home-routing.module';
+import { HeaderComponent } from './components/header/header.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule
+  ],
+  declarations: [HomePage, HeaderComponent]
+})
+export class HomePageModule {}
